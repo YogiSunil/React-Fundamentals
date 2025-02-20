de15566421c395e3f1ce5f3068d152af73bfa87f
Sunil Nath <Sunil.nath@students.dominican.edu>
2025-02-20 13:13:51 -0800

    Header Built

---
+++ src/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Title.css';
+
+function Title(){
+    return(
+        <div>
+            <h1>SFPOPOS</h1>
+            <div className="Title-Subtitle">San Francisco Privately Owned Public Open Spaces</div>
+        </div>
+
+    )
+}
+
+export default Title
